5a8898c1c0ed54328f3d6f1a38306e3bce688e98
einverne <einverne@03d9d21f-f13c-da6c-f0cf-2188766f108d>
2013-05-24 14:34:08 +0000

    domob Layout

--- DestroySquareActionbar/src/cn/blcu/destroysquare/HelpActivity.java
+++ DestroySquareActionbar/src/cn/blcu/destroysquare/HelpActivity.java
@@ -3,9 +3,10 @@ package cn.blcu.destroysquare;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.ActionBar;
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.os.Bundle;
@@ -14,9 +15,9 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
@@ -25,9 +26,12 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.google.analytics.tracking.android.EasyTracker;
 
-public class HelpActivity extends Activity implements OnScrollListener {
+public class HelpActivity extends SherlockActivity implements OnScrollListener {
 
 	private ViewPager mPager;
 	private List<View> listViews;
@@ -69,157 +73,176 @@ public class HelpActivity extends Activity implements OnScrollListener {
 		listViews.add(mInflater.inflate(R.layout.lay3, null));
 		mPager.setAdapter(new MyPagerAdapter(listViews));
 		mPager.setCurrentItem(0);
-		mPager.setOnPageChangeListener(new MyOnPageChangeListener
-
-		());
+		mPager.setOnPageChangeListener(new MyOnPageChangeListener());
 	}
 
-    public class MyPagerAdapter extends PagerAdapter {
-        public List<View> mListViews;
+	public class MyPagerAdapter extends PagerAdapter {
+		public List<View> mListViews;
 
-        public MyPagerAdapter(List<View> mListViews) {
-            this.mListViews = mListViews;
-        }
+		public MyPagerAdapter(List<View> mListViews) {
+			this.mListViews = mListViews;
+		}
 
-        @Override
-        public void destroyItem(View arg0, int arg1, Object arg2) {
-            ((ViewPager) arg0).removeView(mListViews.get(arg1));
-        }
+		@Override
+		public void destroyItem(View arg0, int arg1, Object arg2) {
+			((ViewPager) arg0).removeView(mListViews.get(arg1));
+		}
 
-        @Override
-        public void finishUpdate(View arg0) {
-        }
+		@Override
+		public void finishUpdate(View arg0) {
+		}
 
-        @Override
-        public int getCount() {
-            return mListViews.size();
-        }
+		@Override
+		public int getCount() {
+			return mListViews.size();
+		}
 
-        @Override
-        public Object instantiateItem(View arg0, int arg1) {
-            ((ViewPager) arg0).addView(mListViews.get(arg1), 0);
-            return mListViews.get(arg1);
-        }
+		@Override
+		public Object instantiateItem(View arg0, int arg1) {
+			((ViewPager) arg0).addView(mListViews.get(arg1), 0);
+			if (arg1 == 2) {
+				View view = mListViews.get(arg1);
+				TextView version = (TextView)view.findViewById(R.id.textView_version);
+				version.setText(getAppVersionName(HelpActivity.this));
+			}
+			return mListViews.get(arg1);
+		}
 
-        @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            return arg0 == (arg1);
-        }
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == (arg1);
+		}
 
-        @Override
-        public void restoreState(Parcelable arg0, ClassLoader arg1) {
-        }
+		@Override
+		public void restoreState(Parcelable arg0, ClassLoader arg1) {
+		}
 
-        @Override
-        public Parcelable saveState() {
-            return null;
-        }
+		@Override
+		public Parcelable saveState() {
+			return null;
+		}
 
-        @Override
-        public void startUpdate(View arg0) {
-        }
-    }
+		@Override
+		public void startUpdate(View arg0) {
+		}
+	}
 
-    private void InitImageView() {
-        cursor = (ImageView) findViewById(R.id.cursor);
-        bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.a1)
-                .getWidth();
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int screenW = dm.widthPixels;
-        offset = (screenW / 3 - bmpW) / 2;
-        Matrix matrix = new Matrix();
-        matrix.postTranslate(offset, 0);
-        cursor.setImageMatrix(matrix);
-    }
+	/**
+	 * 返回当前程序版本名
+	 */
+	public static String getAppVersionName(Context context) {
+		String versionName = "";
+		try {
+			// ---get the package info---
+			PackageManager pm = context.getPackageManager();
+			PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+			versionName = pi.versionName;
+			if (versionName == null || versionName.length() <= 0) {
+				return "";
+			}
+		} catch (Exception e) {
+			Log.e("VersionInfo", "Exception", e);
+		}
+		return versionName;
+	}
+	
+	private void InitImageView() {
+		cursor = (ImageView) findViewById(R.id.cursor);
+		bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.a1)
+				.getWidth();
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int screenW = dm.widthPixels;
+		offset = (screenW / 3 - bmpW) / 2;
+		Matrix matrix = new Matrix();
+		matrix.postTranslate(offset, 0);
+		cursor.setImageMatrix(matrix);
+	}
 
-    public class MyOnPageChangeListener implements OnPageChangeListener {
+	public class MyOnPageChangeListener implements OnPageChangeListener {
 
-        int one = offset * 2 + bmpW;
-        int two = one * 2;
-        @Override
-        public void onPageSelected(int arg0) {
-            Animation animation = null;
-            switch (arg0) {
-            case 0:
-                if (currIndex == 1) {
-                    animation = new TranslateAnimation(one, 0, 0, 0);
-                } else if (currIndex == 2) {
-                    animation = new TranslateAnimation(two, 0, 0, 0);
-                }
-                break;
-            case 1:
-                if (currIndex == 0) {
-                    animation = new TranslateAnimation(offset, one, 0, 0);
-                } else if (currIndex == 2) {
-                    animation = new TranslateAnimation(two, one, 0, 0);
-                }
-                break;
-            case 2:
-                if (currIndex == 0) {
-                    animation = new TranslateAnimation(offset, two, 0, 0);
-                } else if (currIndex == 1) {
-                    animation = new TranslateAnimation(one, two, 0, 0);
-                }
-                break;
-            }
-            currIndex = arg0;
-            animation.setFillAfter(true);
-            animation.setDuration(300);
-            cursor.startAnimation(animation);
-        }
+		int one = offset * 2 + bmpW;
+		int two = one * 2;
 
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-        }
+		@Override
+		public void onPageSelected(int arg0) {
+			Animation animation = null;
+			switch (arg0) {
+			case 0:
+				if (currIndex == 1) {
+					animation = new TranslateAnimation(one, 0, 0, 0);
+				} else if (currIndex == 2) {
+					animation = new TranslateAnimation(two, 0, 0, 0);
+				}
+				break;
+			case 1:
+				if (currIndex == 0) {
+					animation = new TranslateAnimation(offset, one, 0, 0);
+				} else if (currIndex == 2) {
+					animation = new TranslateAnimation(two, one, 0, 0);
+				}
+				break;
+			case 2:
+				if (currIndex == 0) {
+					animation = new TranslateAnimation(offset, two, 0, 0);
+				} else if (currIndex == 1) {
+					animation = new TranslateAnimation(one, two, 0, 0);
+				}
+				break;
+			}
+			currIndex = arg0;
+			animation.setFillAfter(true);
+			animation.setDuration(300);
+			cursor.startAnimation(animation);
+		}
 
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-        }
-    }
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
-		
-		ActionBar actionbar = getActionBar();
+
+		ActionBar actionbar = getSupportActionBar();
 		actionbar.setDisplayHomeAsUpEnabled(true);
-		
+
 		InitImageView();
 		InitTextView();
 		InitViewPager();
-		
-	
 
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		// TODO Auto-generated method stub
 		finish();
-		if( keyCode == KeyEvent.KEYCODE_BACK){
-			
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+
 			this.overridePendingTransition(R.anim.in_from_left,
 					R.anim.out_to_right);
 		}
 		return super.onKeyDown(keyCode, event);
-		
-		
+
 	}
-	
 
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem,
 			int visibleItemCount, int totalItemCount) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -240,14 +263,14 @@ public class HelpActivity extends Activity implements OnScrollListener {
 	@Override
 	protected void onStart() {
 		// The rest of your onStart() code.
-	    EasyTracker.getInstance().activityStart(this); // Add this method.F
+		EasyTracker.getInstance().activityStart(this); // Add this method.F
 		super.onStart();
 	}
 
 	@Override
 	protected void onStop() {
 		// The rest of your onStop() code.
-	    EasyTracker.getInstance().activityStop(this); // Add this method.
+		EasyTracker.getInstance().activityStop(this); // Add this method.
 		super.onStop();
 	}
 
@@ -258,9 +281,5 @@ public class HelpActivity extends Activity implements OnScrollListener {
 	// getMenuInflater().inflate(R.menu.help, menu);
 	// return true;
 	// }
-
-	
-
-	
 
 }
