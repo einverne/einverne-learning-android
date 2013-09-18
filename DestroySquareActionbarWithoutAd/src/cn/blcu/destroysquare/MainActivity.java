c0dd4c2388b311cf79b145222d523a8c1bdb806a
einverne <einverne@03d9d21f-f13c-da6c-f0cf-2188766f108d>
2013-09-18 07:01:59 +0000

    remove ads

--- DestroySquareActionbarWithoutAd/src/cn/blcu/destroysquare/MainActivity.java
+++ DestroySquareActionbarWithoutAd/src/cn/blcu/destroysquare/MainActivity.java
@@ -1,12 +1,9 @@
 package cn.blcu.destroysquare;
 
 import net.youmi.android.AdManager;
-import net.youmi.android.banner.AdSize;
-import net.youmi.android.banner.AdView;
 import net.youmi.android.offers.OffersManager;
 import net.youmi.android.offers.PointsManager;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,17 +15,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 import cn.blcu.destroysquaretool.DateTool;
-import cn.domob.android.ads.DomobAdEventListener;
-import cn.domob.android.ads.DomobAdView;
-import cn.domob.android.ads.DomobAdManager.ErrorCode;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
-
 import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.GoogleAnalytics;
 import com.google.analytics.tracking.android.Tracker;
@@ -37,16 +29,13 @@ import com.umeng.update.UmengUpdateAgent;
 import com.umeng.update.UmengUpdateListener;
 import com.umeng.update.UpdateResponse;
 
-public class MainActivity extends SherlockActivity implements View.OnClickListener {
+public class MainActivity extends SherlockActivity implements
+		View.OnClickListener {
 
 	private static final int HELP = 1;
 	private static final int SETTING = 2;
 	private static final int START = 0;
 	private static final String TAG = "EV_DEBUG";
-	public static final String InterstitialPPID = "16TLm5ToApCv4NUH4b_Xhj1z";
-	public static final String PUBLISHER_ID = "56OJzw2IuNXifZqbis";
-	public static final String InlinePPID = "16TLm5ToApCv4NUHYH4Vk63k";
-	protected static final String PASSCODE = "einverne";
 
 	ImageButton help;
 	ImageButton setting;
@@ -55,9 +44,6 @@ public class MainActivity extends SherlockActivity implements View.OnClickListen
 	SharedPreferences.Editor editor;
 	private Tracker myTracker;
 	private GoogleAnalytics mGaInstance;
-	
-	RelativeLayout mAdContainer;
-	DomobAdView mAdview320x50;
 
 	@Override
 	public void onClick(View v) {
@@ -65,38 +51,12 @@ public class MainActivity extends SherlockActivity implements View.OnClickListen
 		switch (tag) {
 		case START:
 			myTracker.sendEvent("ui_action", "button_press", "startGame", null);
-			
-			sharedPreferences = getSharedPreferences("PASSCODE", MODE_PRIVATE);
-			if (sharedPreferences.getBoolean("Forever", false)) {
-				Intent intent = new Intent();
-				intent.setClass(MainActivity.this, GameActivity.class);
-				startActivity(intent);
-				this.overridePendingTransition(R.anim.new_dync_in_from_right,
-						R.anim.new_dync_out_to_left);
-				break;
-			}
-			// 调用spendPoints消费指定金额的积分，这里示例消费100积分
-			if (PointsManager.getInstance(this).spendPoints(10)) {
-				int myPointBalance = PointsManager.getInstance(this)
-						.queryPoints();
-				Log.d("EV_DEBUG", "已消费10积分，当前余额为:" + myPointBalance);
-				Toast.makeText(getApplicationContext(), "已消费10积分，当前余额为:" + myPointBalance, Toast.LENGTH_SHORT).show();
-				Intent intent = new Intent();
-				intent.setClass(MainActivity.this, GameActivity.class);
-				startActivity(intent);
-//				this.overridePendingTransition(R.anim.rotate_in,
-//						R.anim.rotate_out);// 参数:读取res中的XML文件实现效果
-				this.overridePendingTransition(R.anim.new_dync_in_from_right,
-						R.anim.new_dync_out_to_left);// 参数:读取res中的XML文件实现效果
-				// 摇摆
-			} else {
-				Toast.makeText(MainActivity.this, "消费积分失败(积分余额不足),请去获取积分,跳转到积分墙",
-						Toast.LENGTH_SHORT).show();
-				myTracker.sendEvent("ui_action", "no_jifen", "getJifen", null);
-				// 调用showOffersWall显示全屏的积分墙界面
-				OffersManager.getInstance(this).showOffersWall();
-			}
 
+			Intent intent = new Intent();
+			intent.setClass(MainActivity.this, GameActivity.class);
+			startActivity(intent);
+			this.overridePendingTransition(R.anim.new_dync_in_from_right,
+					R.anim.new_dync_out_to_left);
 			break;
 		case HELP:
 			myTracker.sendEvent("ui_action", "button_press", "helpActivity",
@@ -104,8 +64,8 @@ public class MainActivity extends SherlockActivity implements View.OnClickListen
 			Intent intent_2 = new Intent();
 			intent_2.setClass(MainActivity.this, HelpActivity.class);
 			startActivity(intent_2);
-			MainActivity.this.overridePendingTransition(R.anim.new_dync_in_from_right,
-					R.anim.new_dync_out_to_left);
+			MainActivity.this.overridePendingTransition(
+					R.anim.new_dync_in_from_right, R.anim.new_dync_out_to_left);
 			// 参数:读取res中的XML文件实现效果
 			break;
 		case SETTING:
@@ -114,8 +74,8 @@ public class MainActivity extends SherlockActivity implements View.OnClickListen
 			Intent intent_3 = new Intent();
 			intent_3.setClass(MainActivity.this, SettingsActivity.class);
 			startActivity(intent_3);
-			MainActivity.this.overridePendingTransition(R.anim.new_dync_in_from_right,
-					R.anim.new_dync_out_to_left);// 参数:读取res中的XML文件实现效果
+			MainActivity.this.overridePendingTransition(
+					R.anim.new_dync_in_from_right, R.anim.new_dync_out_to_left);// 参数:读取res中的XML文件实现效果
 			break;
 		default:
 			break;
@@ -150,94 +110,18 @@ public class MainActivity extends SherlockActivity implements View.OnClickListen
 		start.setOnClickListener(this);
 		help.setOnClickListener(this);
 		setting.setOnClickListener(this);
-		
-		LinearLayout linear_start = (LinearLayout)findViewById(R.id.linear_play);
+
+		LinearLayout linear_start = (LinearLayout) findViewById(R.id.linear_play);
 		linear_start.setTag(START);
 		linear_start.setOnClickListener(this);
-		
-		LinearLayout linear_help = (LinearLayout)findViewById(R.id.linear_how);
+
+		LinearLayout linear_help = (LinearLayout) findViewById(R.id.linear_how);
 		linear_help.setTag(HELP);
 		linear_help.setOnClickListener(this);
-		LinearLayout linear_setting = (LinearLayout)findViewById(R.id.linear_setting);
+		LinearLayout linear_setting = (LinearLayout) findViewById(R.id.linear_setting);
 		linear_setting.setTag(SETTING);
 		linear_setting.setOnClickListener(this);
-		
 
-		iniADs();
-		iniDomobInlineAD();
-		sharedPreferences = this.getSharedPreferences("Date",
-				Context.MODE_PRIVATE);
-		editor = sharedPreferences.edit();
-		if (sharedPreferences.getString("isFirst", "first").equals("first")) {
-			// 调用awardPoints可以奖励指定金额的积分，这里示例奖励50积分
-			PointsManager.getInstance(this).awardPoints(50);
-			int myPointBalance = PointsManager.getInstance(this).queryPoints();
-			Toast.makeText(this, "奖励50积分，当前余额:" + myPointBalance,
-					Toast.LENGTH_SHORT).show();
-			editor.putString("isFirst", "not");
-			editor.commit();
-		}
-		
-		
-		mAdview320x50.setAdEventListener(new DomobAdEventListener() {
-			
-			@Override
-			public void onDomobLeaveApplication(DomobAdView arg0) {
-				
-			}
-			
-			@Override
-			public void onDomobAdReturned(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public Context onDomobAdRequiresCurrentContext() {
-				// TODO Auto-generated method stub
-				return MainActivity.this;
-			}
-			
-			@Override
-			public void onDomobAdOverlayPresented(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onDomobAdOverlayDismissed(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onDomobAdFailed(DomobAdView arg0, ErrorCode arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onDomobAdClicked(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		mAdContainer.addView(mAdview320x50);
-	}
-
-	private void iniDomobInlineAD() {
-		mAdContainer = (RelativeLayout)findViewById(R.id.domobAdcontainer);
-		mAdview320x50 = new DomobAdView(this, PUBLISHER_ID, InlinePPID, DomobAdView.INLINE_SIZE_320X50);
-	}
-
-	private void iniADs() {
-		// 实例化广告条
-		AdView adView = new AdView(this, AdSize.SIZE_320x50);
-		// 获取要广告条的布局
-		LinearLayout adLayout = (LinearLayout) findViewById(R.id.adLayout);
-		// 将广告条加入到布局中
-		adLayout.addView(adView);
 	}
 
 	@Override
@@ -277,103 +161,6 @@ public class MainActivity extends SherlockActivity implements View.OnClickListen
 			startActivity(intent_3);
 			MainActivity.this.overridePendingTransition(R.anim.rotate_in,
 					R.anim.rotate_out);// 参数:读取res中的XML文件实现效果
-			break;
-		case R.id.action_eachday:
-			myTracker.sendEvent("ui_action", "menu_press", "eachday", null);
-			if (!DateTool.getTodayDate().equals(
-					sharedPreferences.getString("date",
-							DateTool.getDateTodayMinusDay(1)))) {
-				// 调用awardPoints可以奖励指定金额的积分，这里示例奖励50积分
-				PointsManager.getInstance(this).awardPoints(50);
-				int myPointBalance = PointsManager.getInstance(this)
-						.queryPoints();
-				Toast.makeText(this, "奖励50积分，当前余额:" + myPointBalance,
-						Toast.LENGTH_SHORT).show();
-				Log.d("EV_DEBUG", "已成功奖励50积分，当前余额为:" + myPointBalance);
-				editor.putString("date", DateTool.getTodayDate());
-				Log.d(TAG, DateTool.getTodayDate());
-				editor.commit();
-			} else {
-				int myPointBalance = PointsManager.getInstance(this)
-						.queryPoints();
-				Toast.makeText(this, "今天已经签过到,明天再来吧!当前余额:"+myPointBalance, Toast.LENGTH_SHORT)
-						.show();
-			}
-			break;
-		case R.id.action_jifen:
-			myTracker.sendEvent("ui_action", "menu_press", "getJifen", null);
-			// 调用showOffersWall显示全屏的积分墙界面
-			OffersManager.getInstance(this).showOffersWall();
-			break;
-		case R.id.update:
-			UmengUpdateAgent.update(getApplication());
-			UmengUpdateAgent.setUpdateAutoPopup(false);
-			UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
-				
-				@Override
-				public void onUpdateReturned(int updateStatus, UpdateResponse updateInfo) {
-					switch (updateStatus) {
-		            case 0: // has update
-		                UmengUpdateAgent.showUpdateDialog(MainActivity.this, updateInfo);
-		                break;
-		            case 1: // has no update
-		                Toast.makeText(MainActivity.this, "没有更新", Toast.LENGTH_SHORT)
-		                        .show();
-		                break;
-		            case 2: // none wifi
-		                Toast.makeText(MainActivity.this, "没有wifi连接， 只在wifi下更新", Toast.LENGTH_SHORT)
-		                        .show();
-		                break;
-		            case 3: // time out
-		                Toast.makeText(MainActivity.this, "超时", Toast.LENGTH_SHORT)
-		                        .show();
-		                break;
-		            }
-				}
-			});
-			UmengUpdateAgent.setOnDownloadListener(new UmengDownloadListener() {
-				
-				@Override
-				public void OnDownloadEnd(int result) {
-			        if (result == 1) {
-			        	Toast.makeText(MainActivity.this, "下载成功" , Toast.LENGTH_SHORT).show();
-					}else{
-						Toast.makeText(MainActivity.this, "下载失败" , Toast.LENGTH_SHORT).show();
-					}
-				}
-			});
-			break;
-		case R.id.passcode:
-			LayoutInflater inflater = LayoutInflater.from(getApplication());
-			final View dialogview = inflater.inflate(R.layout.dialogview, null);
-			new AlertDialog.Builder(MainActivity.this).setTitle("请输入passcode:")
-			.setView(dialogview)
-			.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					EditText et_passcode = (EditText)dialogview.findViewById(R.id.et_passcode);	
-					Editable editable = et_passcode.getText();
-					if (editable.toString().matches("")) {
-						Toast.makeText(getApplication(), "请输入", Toast.LENGTH_SHORT).show();
-					}else{
-						String passcode = editable.toString();
-						if (passcode.equals(PASSCODE)) {
-							sharedPreferences = getSharedPreferences("PASSCODE", MODE_PRIVATE);
-							editor = sharedPreferences.edit();
-							editor.putBoolean("Forever", true);
-							editor.commit();
-						}
-					}
-				}
-			})
-			.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.dismiss();
-				}
-			}).create().show();
 			break;
 		default:
 			break;
--- DestroySquareActionbarWithoutAd/src/cn/blcu/destroysquare/SettingsActivity.java
+++ DestroySquareActionbarWithoutAd/src/cn/blcu/destroysquare/SettingsActivity.java
@@ -8,29 +8,21 @@ import android.view.KeyEvent;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.RelativeLayout;
-import cn.domob.android.ads.DomobAdEventListener;
-import cn.domob.android.ads.DomobAdManager.ErrorCode;
-import cn.domob.android.ads.DomobAdView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.ActionProvider;
-import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class SettingsActivity extends SherlockActivity {
 	public static final String PUBLISHER_ID = "56OJzw2IuNXifZqbis";
 	public static final String InlinePPID = "16TLm5ToApCv4NUHYH4Vk63k";
-	
+
 	boolean bgflag = true;
 	SharedPreferences.Editor editor;
 	SharedPreferences sharedPreferences;
 
 	boolean yxflag = true;
-	RelativeLayout ad;
-	DomobAdView domobAdView;	//300*25
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +33,14 @@ public class SettingsActivity extends SherlockActivity {
 		CheckBox bg = (CheckBox) this.findViewById(R.id.CheckBox_bg);
 		CheckBox yx = (CheckBox) this.findViewById(R.id.CheckBox_yx);
 		CheckBox xz = (CheckBox) this.findViewById(R.id.checkBox_xz);
-		ad = (RelativeLayout)findViewById(R.id.ad);
-		domobAdView = new DomobAdView(this, PUBLISHER_ID, InlinePPID, DomobAdView.INLINE_SIZE_320X50);
-		
+
 		sharedPreferences = this.getSharedPreferences("flag",
 				Context.MODE_PRIVATE);
 		editor = sharedPreferences.edit();
 		bg.setChecked(sharedPreferences.getBoolean("bgflag", true));
 		yx.setChecked(sharedPreferences.getBoolean("yxflag", true));
 		xz.setChecked(sharedPreferences.getBoolean("xzflag", false));
-		
+
 		bg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
@@ -70,7 +60,7 @@ public class SettingsActivity extends SherlockActivity {
 				editor.commit();
 			}
 		});
-		
+
 		xz.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
@@ -80,73 +70,27 @@ public class SettingsActivity extends SherlockActivity {
 				editor.commit();
 			}
 		});
-		
-		domobAdView.setAdEventListener(new DomobAdEventListener() {
-			
-			@Override
-			public void onDomobLeaveApplication(DomobAdView arg0) {
-				
-			}
-			
-			@Override
-			public void onDomobAdReturned(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public Context onDomobAdRequiresCurrentContext() {
-				// TODO Auto-generated method stub
-				return SettingsActivity.this;
-			}
-			
-			@Override
-			public void onDomobAdOverlayPresented(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onDomobAdOverlayDismissed(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onDomobAdFailed(DomobAdView arg0, ErrorCode arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onDomobAdClicked(DomobAdView arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		ad.addView(domobAdView);
+
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.help, menu);
-//		return true;
-//	}
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.help, menu);
+	// return true;
+	// }
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		// TODO Auto-generated method stub
 		finish();
-		if(keyCode == KeyEvent.KEYCODE_BACK){
-			
-			SettingsActivity.this.overridePendingTransition(R.anim.in_from_left,
-					R.anim.out_to_right);
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+			SettingsActivity.this.overridePendingTransition(
+					R.anim.in_from_left, R.anim.out_to_right);
 		}
 		return super.onKeyDown(keyCode, event);
-		
-		
+
 	}
 
 	@Override
@@ -167,19 +111,15 @@ public class SettingsActivity extends SherlockActivity {
 	@Override
 	protected void onStart() {
 		// The rest of your onStart() code.
-	    EasyTracker.getInstance().activityStart(this); // Add this method.
+		EasyTracker.getInstance().activityStart(this); // Add this method.
 		super.onStart();
 	}
 
 	@Override
 	protected void onStop() {
 		// The rest of your onStop() code.
-	    EasyTracker.getInstance().activityStop(this); // Add this method. 
+		EasyTracker.getInstance().activityStop(this); // Add this method.
 		super.onStop();
 	}
-
-	
-	
-	
 
 }
